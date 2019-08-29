4e4c4d3bb00d44c84a2b69bd8a3c719e5f0c1525
Mikołaj Lechtański <mikolec@gmail.com>
2019-08-29 08:33:21 +0200

    Updates

---
+++ precache-manifest.0ae732e4324e6af589ef2a8ad397684b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0509ecdebf8223a659c8bd4c32b91ace",
+    "url": "/xtm-intl/index.html"
+  },
+  {
+    "revision": "2134e20209eef9a5eaa7",
+    "url": "/xtm-intl/static/css/main.0ff91870.chunk.css"
+  },
+  {
+    "revision": "73cc54c803f29b5efa4a",
+    "url": "/xtm-intl/static/js/2.28b0ab0a.chunk.js"
+  },
+  {
+    "revision": "2134e20209eef9a5eaa7",
+    "url": "/xtm-intl/static/js/main.6b9cf4f4.chunk.js"
+  },
+  {
+    "revision": "e20e53eaff8ff875ed7f",
+    "url": "/xtm-intl/static/js/runtime~main.314a0512.js"
+  }
+]);
