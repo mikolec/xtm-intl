ead01ad288d379d1dadc0b8e3cc8c2e73a9edbca
Mikołaj Lechtański <mikolec@gmail.com>
2019-08-29 08:22:16 +0200

    project created v.1.0.0

--- README.md
+++ README.md
@@ -1,68 +1,13 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# XTM International - recruitment task
 
-## Available Scripts
+#### Implement a code that will look for the top 10 results in the Wikipedia API (sample query: https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=%22Liverpool%22&srlimit=10)
 
-In the project directory, you can run:
+#### Requirements:
+1. The view should contain the “search phrase” input field, “replace with” input field and three buttons: search, replace, replace all.
+2. Results should be displayed as a list and should include result title and snipped fields.
+3. Search matches within each result snipped should be highlighted.
+4. The request should be invoked on search button click and while typing in search field (not on every key pressed)
+5. Replace should replace the first highlighted search match with a phrase taken from “replace with” field.
+6. Replace all should replace every highlighted search match.
 
-### `npm start`
-
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+#### Allowed technologies: VanillaJS / AngularJS / React  
---
+++ src/components/App.js
@@ -0,0 +1,150 @@
+import React, { Component } from "react";
+import "./App.css";
+
+const url_base = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json";
+
+class App extends Component {
+  state = {
+    results: [],
+    searchInput: "Balboa",
+    searchText: "",
+    replaceInput: ""
+  };
+
+  getResults = (str, limit = 10) => {
+    var url = `${url_base}&srsearch="${str}"&srlimit=${limit}&origin=*`;
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response;
+        }
+        throw new Error(response.status);
+      })
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data.query.search);
+
+        var results = data.query.search.map(({ snippet, title, pageid }) => ({ snippet, title, pageid }));
+
+        this.setState({ results });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  replaceWith = str => {
+    // console.log(this.state.searchText, "i");
+    var regex = RegExp(this.state.searchText, "i");
+    var firstIndex = this.state.results.findIndex(result => {
+      return regex.test(result.snippet);
+    });
+    // console.log(firstIndex);
+    if (firstIndex !== -1) {
+      var newSnippet = this.state.results[firstIndex].snippet.replace(regex, str);
+      console.log(newSnippet);
+
+      var newResults = this.state.results.map((result, index) => {
+        if (index === firstIndex) {
+          result.snippet = newSnippet;
+        }
+        return result;
+      });
+
+      this.setState({ results: newResults });
+    }
+  };
+
+  replaceAllWith = str => {
+    var regex = RegExp(this.state.searchText, "ig");
+
+    var newResults = this.state.results.map((result, index) => {
+      // if (index === firstIndex) {
+      //   result.snippet = newSnippet;
+      // }
+      result.snippet = result.snippet.replace(regex, str);
+
+      return result;
+    });
+
+    this.setState({ results: newResults });
+  };
+
+  handleInputChange = e => {
+    // console.log(e.target.name);
+    // console.log(e.target.value);
+
+    //e.target.value !== ""
+    if (true) {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    }
+  };
+
+  handleButtonClick = e => {
+    var buttonName = e.target.name;
+    switch (buttonName) {
+      case "searchBtn":
+        // console.log("search button clicked");
+        this.getResults(this.state.searchInput);
+        this.setState(prevState => {
+          return {
+            searchText: prevState.searchInput
+            // searchInput: ""
+          };
+        });
+
+        break;
+      case "replaceBtn":
+        // console.log("replace button clicked");
+        this.replaceWith(this.state.replaceInput);
+        break;
+      case "replaceAllBtn":
+        // console.log("replaceAll button clicked");
+        this.replaceAllWith(this.state.replaceInput);
+        break;
+      default:
+        console.log("unknown button clicked");
+        break;
+    }
+  };
+
+  render() {
+    var { searchText, searchInput, replaceInput, results } = this.state;
+    var Results = results.map(({ snippet, title, pageid }) => {
+      // return <li key={pageid}>{snippet}</li>;
+      return <li key={pageid} dangerouslySetInnerHTML={{ __html: snippet }}></li>;
+    });
+    return (
+      <div className="wrapper">
+        <div className="inputWrapper">
+          <span>Search:</span>
+          <input type="text" name="searchInput" onChange={this.handleInputChange} value={searchInput} />
+          <button name="searchBtn" onClick={this.handleButtonClick}>
+            Search
+          </button>
+        </div>
+        <div className="inputWrapper">
+          <span>Replace with:</span>
+          <input type="text" name="replaceInput" onChange={this.handleInputChange} value={replaceInput} />
+          <button name="replaceBtn" onClick={this.handleButtonClick}>
+            Replace
+          </button>
+          <button name="replaceAllBtn" onClick={this.handleButtonClick}>
+            Replace All
+          </button>
+        </div>
+
+        <h2>
+          Search results for: <em>{searchText}</em>
+        </h2>
+
+        <ul className="listWrapper">{Results}</ul>
+      </div>
+    );
+  }
+}
+
+export default App;
